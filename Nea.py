2dbdb03d645c1daece8ba0c89b8e0171005f1e48
jiangnanqing <jiangnanqing@gmail.com>
2017-03-13 22:08:20 +0800

    re-write nea with requests and beautifulsoup lib

--- Nea.py
+++ Nea.py
@@ -1,21 +1,18 @@
-import urllib2
-import xmltodict
-import os
-from datetime import datetime
+import requests
+import json
+from bs4 import BeautifulSoup
+print 'Start process'
 key = '781CF461BB6606ADEA6B1B4F3228DE9D5024E401E592608F'
-url = 'http://api.nea.gov.sg/api/WebAPI?dataset=2hr_nowcast&keyref=' + key
+url = 'http://api.nea.gov.sg/api/WebAPI/?dataset=2hr_nowcast&keyref=' + key
 print url
-rain = ['HG','HR','HS','LR','LS','PS','RA','SH','SR','WS']
-response = urllib2.urlopen(url)
-webcontent = response.read()
-print webcontent
-dom = xmltodict.parse(webcontent)
-print dom['channel']['item']['weatherForecast']['area'][1]
-filename = '~/Dropbox/project/busstoppy/data/weather.txt'
-with open(os.path.expanduser(filename), 'w') as f:
-    f.write('timestamp:{:%H:%M:%S}'.format(datetime.now()))
-    f.write('\n')
-    for item in dom['channel']['item']['weatherForecast']['area']:
-        f.write(item['@name'] + ', ' + item['@forecast'] + '\n')
-f.close()
+s = requests.Session()
+r = s.get(url)
+soup = BeautifulSoup(r.text, 'xml')
+w = soup.find_all('area')
+weather = []
+for item in w:
+    forecast = item.get('forecast')
+    name = item.get('name')
+    weather.append([forecast, name])
+# rain = ['HG','HR','HS','LR','LS','PS','RA','SH','SR','WS']
 print ("process completed")
